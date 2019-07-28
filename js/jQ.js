5e9f2b486ac92cbc049bf517f223d595c1f1341d
RyotaHoshi <ranpo.kogoro@gmail.com>
2019-07-28 18:17:01 +0900

    gameReadme+

--- js/jQ.js
+++ js/jQ.js
@@ -8,17 +8,19 @@
 $(function() {
 
 $('#people').slideUp(3000);
-$('.gameP').fadeOut(1500);
 $('.gameP').fadeIn(1500);
-$('.gameP2').fadeOut(1500);
 $('.gameP2').fadeIn(1500);
-$('.gameP3').fadeOut(1500);
 $('.gameP3').fadeIn(1500);
-$('.gameP4').fadeOut(1500);
 $('.gameP4').fadeIn(1500);
-$('p').fadeOut(1500);
-$('p').fadeIn(1500);
 // $('body1').fadeOut(1500);もともとcssでアニメーション設定したものは使えない？
+//('slow')という引数の表記も可能
 
-
+// イベントの基本形
+// $().イベント名(function() {
+  //イベント時に実行したい処理
+// });
+// 上記を踏まえて下記クリックイベントの処理の書き方
+$('.readme').click(function() {
+  $('.Sentence').fadeIn('slow');
+});
 });
