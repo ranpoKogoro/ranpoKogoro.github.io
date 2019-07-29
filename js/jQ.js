acb473756bcb81e5b1a62fab8593906f4546a1b3
RyotaHoshi <ranpo.kogoro@gmail.com>
2019-07-29 21:56:14 +0900

    maked ごゆっくりどうぞ

--- js/jQ.js
+++ js/jQ.js
@@ -6,31 +6,28 @@
 // セレクタ(選択したものを)メソッド(どうするか)
 
 $(function() {
-
 $('.gameP').fadeIn(1500);
 $('.gameP2').fadeIn(1500);
 $('.gameP3').fadeIn(1500);
 $('.gameP4').fadeIn(1500);
-// $('.body1').fadeOut(1500);
-// $('.body1').fadeIn(1500);
-//('slow')という引数の表記も可能
 window.onload = () => {
    let today = new Date();
    let welcomeWord = (today.getFullYear() + "年" + (today.getMonth()+1) + "月" + today.getDate() +"日"+ today.getHours() + "時" +today.getMinutes()+ "分" + today.getSeconds() + "秒のお客様です");
    let nowHour = today.getHours();
+   let nowMinute = today.getMinutes();
    console.log(welcomeWord);
    console.log(nowHour);
    if ( nowHour > 4 && nowHour <= 10 ) {
-     $('#people').html('おはよう！　' + nowHour + '時だね。今日も頑張ろう！');
+     $('#people').html('おはよう！ただいまの時刻は' + nowHour + '時' + nowMinute + '分だよ。<br>今日も頑張ろう！');
      $('#people').fadeIn(1500);
    } else if( nowHour > 10 && nowHour <= 17 ) {
-     $('#people').html('こんにちは！　' + nowHour + '時だね。ちゃんと休憩取ってる？');
+     $('#people').html('こんにちは！ただいまの時刻は' + nowHour + '時' + nowMinute + '分だね。<br>ちゃんと休憩取ってる？');
      $('#people').fadeIn(1500);
    } else if( nowHour > 17 && nowHour <= 23 ){
-     $('#people').html('こんばんは！　' + nowHour + 'だね。今日もお疲れ様！');
+     $('#people').html('こんばんは！ただいまの時刻は' + nowHour + '時' + nowMinute + '分だよ〜。<br>今日もお疲れ！');
      $('#people').fadeIn(1500);
    }else{
-     $('#people').html( '深夜' + nowHour + '時です。夜更かししすぎないでね...');
+     $('#people').html( 'ふにゃふにゃ..ただいまの時刻は深夜' + nowHour + '時' + nowMinute + '分...<br>夜更かししすぎないでね...ふにゃふにゃ');
      $('#people').fadeIn(1500);
    }
 }
@@ -39,6 +36,9 @@ window.onload = () => {
   //イベント時に実行したい処理
 // });
 // 上記を踏まえて下記クリックイベントの処理の書き方
+$('#people').click(function() {
+  $('#people').html('ごゆっくりどうぞ').css('background-color','rgba(255,215,0,0.5)').slideUp(3000);
+});
 $('.readme').click(function() {
   $('.readme').hide();
   $('.readmeClose').show();
